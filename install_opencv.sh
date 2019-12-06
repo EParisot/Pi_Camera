12ef0c22452efb742a4627674689235a8c2bd6ed
eparisot <eparisot@student.42.fr>
2019-12-06 14:47:32 +0100

    install mod

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,6 +1,6 @@
-pip3 install opencv-python
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libjasper-dev
-sudo apt-get install libqtgui4
-sudo apt-get install libqt4-test
-sudo apt-get install python3-pyqt5
+pip3 install opencv-python -y
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install libjasper-dev -y
+sudo apt-get install libqtgui4 -y
+sudo apt-get install libqt4-test -y
+sudo apt-get install python3-pyqt5 -y
